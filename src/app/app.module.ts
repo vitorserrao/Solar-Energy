202a16d0b6e177a5f2f0f71fccbfa2201b64bddb
Vitor Serrão <vitorwss@gmail.com>
2022-05-29 09:00:25 -0300

    Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-import { CadastroUnidadeComponent } from './components/pages/cadastro-unidade/cadastro-unidade.component';
 import { GeracaoEnergiaComponent } from './components/pages/geracao-energia/geracao-energia.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './service/auth.service';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AuthGuardService } from './service/auth-guard.service';
+import { UnidadesModule } from './components/pages/unidades/unidades.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    CadastroUnidadeComponent,
     GeracaoEnergiaComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UnidadesModule,
+    HttpClientModule,
+  ],
   providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent],
 })
